1c93bba28b093a0829fea771885d59c2691bea59
anaghrao-99 <anaghrao@gmail.com>
2020-04-20 22:43:52 +0530

    marks upload

--- backend.py
+++ backend.py
@@ -908,8 +908,34 @@ def correct():
             if '.txt' in files[i]:
                 answer_file = files[i]
 
-        pipe1 = subprocess.check_call(["python", "semantic.py" , string, path + answer_file], cwd= initial_directory + '/automated_correction_module/')
-        time.sleep(5)
+        # pipe1 = subprocess.check_call(["python", "semantic.py" , string, path + answer_file], cwd= initial_directory + '/automated_correction_module/')
+        # time.sleep(5)
+        with open('marks.txt', "w") as f:
+            pipe1 = subprocess.check_call(["python", "semantic.py" , string, path + answer_file],stdout= f, cwd= initial_directory + '/automated_correction_module/')
+
+
+        file = open('marks.txt', "r")
+        counter = 0
+        for line in file:
+            if(counter > 1):
+                break
+            if(counter == 0):
+                files = line[1:len(line)-2].split(',')
+
+            if(counter == 1):
+                marks = line[2:len(line)-2].split()
+
+
+            counter += 1
+
+        maxMarks= request.get_data()
+        print(maxMarks)
+
+        for i in range(len(files)):
+            data[files[i].split('_')[0]] = round(float(marks[i+1])*int(maxMarks))
+
+        data['msg']= 'done'
+        # time.sleep(5)
         return data
     
 
